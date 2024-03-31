44277411b18e24eef612ce0d08edbb66bc1d3353
sssssaharat <saharatseerakoon@gmail.com>
2024-03-31 15:13:57 +0700

    Add component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
-
+import Dashboard from "./component/Dashboard/Dashboard";
+import Anlytics from "./component/Analytics/Analytics";
+import Journal from "./component/ Journal/ Journal";
 function App() {
-  
-
   return (
     <>
-     
+      <Dashboard />
+      <Anlytics />
+      <Journal/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
